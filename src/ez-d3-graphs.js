a47dedd0c0a22d0ef9c9d1f30c456430b4d11a6e
Mike Estes <mikejestes@gmail.com>
2012-10-24 17:07:06 -0700

    Stacked line graph

--- src/ez-d3-graphs.js
+++ src/ez-d3-graphs.js
@@ -468,6 +468,84 @@
         }
     });
 
+    expose.StackedLineGraph = function (data, options) {
+        var defaults = {
+            color: '#000',
+        };
+
+        this.data = data;
+        this.options = extend(defaults, options);
+    };
+    expose.StackedLineGraph.prototype = extend(new BaseGraph(), {
+        maxYValue: function(data) {
+
+            var max = 0;
+            data.map(function(d) {
+                max = d3.max([d3.sum(d3.map(d).values()), max]);
+            });
+
+            return max;
+        },
+        render: function (svg, props) {
+
+            var self = this;
+
+            var x = d3.time.scale()
+                .range([props.leftGutter, props.width - props.rightGutter]);
+
+            var y = d3.scale.linear()
+                .range([props.height - props.bottomGutter, props.topGutter]);
+
+            var color = d3.scale.category20();
+
+            var area = d3.svg.area()
+                .x(function(d, i) { return x(i); })
+                .y0(function(d) { return y(d.y0); })
+                .y1(function(d) { return y(d.y0 + d.y); });
+
+            var stack = d3.layout.stack()
+                .values(function(d) { return d.values; });
+
+            color.domain(d3.keys(this.data[0]));
+
+            var max = 0;
+
+            var browsers = stack(color.domain().map(function(name) {
+                return {
+                    name: name,
+                    values: self.data.map(function(d) {
+                        max = d3.max([d3.sum(d3.map(d).values()), max]);
+                        return {y: d[name]};
+                    })
+                };
+            }));
+
+            x.domain([0, this.data.length]);
+            y.domain([0, max]);
+
+            var browser = svg.selectAll(".browser")
+                .data(browsers)
+                .enter().append("g")
+                .attr("class", "browser");
+
+            browser.append("path")
+              .attr("class", "area")
+              .attr('title', function(d) {return d.name; })
+              .attr("d", function(d) { return area(d.values); })
+              .style("fill", function(d) { return color(d.name); });
+
+            /*
+            browser.append("text")
+              .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+              .attr("transform", function(d, i) { return "translate(" + x(i) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+              .attr("x", -6)
+              .attr("dy", ".35em")
+              .text(function(d) { return d.name; });
+            */
+
+        }
+    });
+
     expose.TimeValueGraph = function (data, options) {
         var defaults = {
             color: '#000',
